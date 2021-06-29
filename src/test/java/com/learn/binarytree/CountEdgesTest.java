ad6bd906545e6d4994c6587568f29501aba0584e
yossi78 <yossi78@gmail.com>
2021-06-29 14:23:22 +0300

    Count Edges

---
+++ src/test/java/com/learn/binarytree/CountEdgesTest.java
@@ -0,0 +1,47 @@
+package com.learn.binarytree;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+
+public class CountEdgesTest {
+
+
+    @Test
+    public void countEvenTreeTest(){
+        Integer[]arr={80,40,300,20,50,200,500,10,25,45,60,100,250,400,700};
+        TreeNode head=BinaryTreeUtil.addNode(arr);
+        int count=BinaryTreeUtil.countEdges(head);
+        Assert.assertEquals(14,count);
+    }
+
+
+    @Test
+    public void countRightestTreeTest(){
+        Integer[]arr={80,40,300,500,700,900};
+        TreeNode head=BinaryTreeUtil.addNode(arr);
+        int count=BinaryTreeUtil.countEdges(head);
+        Assert.assertEquals(5,count);
+    }
+
+    @Test
+    public void countLeftestTreeTest(){
+        Integer[]arr={900,700,500,300,80};
+        TreeNode head=BinaryTreeUtil.addNode(arr);
+        int count=BinaryTreeUtil.countEdges(head);
+        Assert.assertEquals(4,count);
+    }
+
+
+    @Test
+    public void countBrokenTreeTest(){
+        Integer[]arr={80,300,500,400,200,100};
+        TreeNode head=BinaryTreeUtil.addNode(arr);
+        int count=BinaryTreeUtil.countEdges(head);
+        Assert.assertEquals(5,count);
+    }
+
+
+
+}
